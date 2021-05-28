828789665937a340bde546bfec22090a138e6390
Ethan Pressley <epressle@testium.cs.uoregon.edu>
2021-05-27 20:48:38 -0700

    fixed test case, made changes to Dockerfile as per comment

--- brevets/tests/db_test.py
+++ brevets/tests/db_test.py
@@ -1,12 +1,12 @@
 import nose
 from pymongo import MongoClient
 import os
-from flask_brevets import insert_to_db
-
+import logging
+from bson.objectid import ObjectId
 
 def test_db_single():
     client = MongoClient('mongodb://' + os.environ['MONGODB_HOSTNAME'], 27017)
-    db = client.brevetdb
+    db = client.tempdb
 
     test_dict_single = {
         'brev_distance': '400',
@@ -14,26 +14,20 @@ def test_db_single():
         'close_times': ['2021-01-01T01:00'],
         'kms': [0]
     }
+    client.drop_database('tempdb')
 
-    res = insert_to_db(test_dict_single)
+    res = db.tempdb.insert_one(test_dict_single)
+    ID = ObjectId(res.inserted_id)
     # check for valid insertion
     assert(res.acknowledged)
-    assert(db.brevetdb.find_one({'_id': res.inserted_id} is not None))
+    assert(db.tempdb.find_one({'_id': ID}) is not None)
     # delete from database
-    assert(db.brevetdb.find_one_and_delete({'_id': res.inserted_id} is not None))
-    assert(db.brevetdb.find_one({'_id': res.inserted_id} is None))
-    # try a bigger dictionary
-    res = insert_to_db(test_dict_multi)
-    # check for valid insertion
-    assert(res.acknowledged)
-    assert(db.brevetdb.find_one({'_id': res.inserted_id} is not None))
-    # delete from database
-    assert(db.brevetdb.find_one_and_delete({'_id': res.inserted_id} is not None))
-    assert(db.brevetdb.find_one({'_id': res.inserted_id} is None))
+    assert(db.tempdb.find_one_and_delete({'_id': ID}) is not None)
+    assert(db.tempdb.find_one({'_id': ID}) is None)
 
 def test_db_multi():
     client = MongoClient('mongodb://' + os.environ['MONGODB_HOSTNAME'], 27017)
-    db = client.brevetdb
+    db = client.tempdb
 
     test_dict_multi = {
         'brev_distance': '1000',
@@ -41,11 +35,13 @@ def test_db_multi():
         'close_times': ['2021-01-01T01:00', '2021-01-01T06:40', '2021-01-01T13:30', '2021-01-02T02:40', '2021-01-03T00:45', '2021-01-04T03:00'],
         'kms': ['0', '100', '200', '400', '700', '1000']
     }
+    client.drop_database('tempdb')
 
-    res = insert_to_db(test_dict_multi)
+    res = db.tempdb.insert_one(test_dict_multi)
+    ID = ObjectId(res.inserted_id)
     # check for valid insertion
     assert(res.acknowledged)
-    assert(db.brevetdb.find_one({'_id': res.inserted_id} is not None))
+    assert(db.tempdb.find_one({'_id': ID}) is not None)
     # delete from database
-    assert(db.brevetdb.find_one_and_delete({'_id': res.inserted_id} is not None))
-    assert(db.brevetdb.find_one({'_id': res.inserted_id} is None))
+    assert(db.tempdb.find_one_and_delete({'_id': ID}) is not None)
+    assert(db.tempdb.find_one({'_id': ID}) is None)
